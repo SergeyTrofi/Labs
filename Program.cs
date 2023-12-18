df767c006875f1978d6de865b8e873e6062a961f
Sergey Trofimov <strofimov865@gmail.com>
2023-12-18 23:25:29 +0500

    Labs5

--- Program.cs
+++ Program.cs
@@ -1,104 +1,138 @@
-﻿static void Main()
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Security;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+
+namespace Labs
 {
-    string input = Console.ReadLine();
-    string rpn = InfixToRPN(input);
-
-    Console.WriteLine("Выражение в обратной польской записи (ОПЗ):");
-    Console.WriteLine(rpn);
-
-    double result = CalculateRPN(rpn);
-    Console.WriteLine("Результат: " + result);
-}
-
-static string InfixToRPN(string infix)
-{
-    string[] tokens = infix.Split(' ');
-    string rpn = "";
-    Stack<string> operatorStack = new Stack<string>();
-
-    Dictionary<string, int> precedence = new Dictionary<string, int>
-        {
-            { "+", 1 },
-            { "-", 1 },
-            { "*", 2 },
-            { "/", 2 }
-        };
-
-    foreach (string token in tokens)
+    class Program
     {
-        if (double.TryParse(token, out double number))
+        public class String
         {
-            rpn += number + " ";
+            public string input;
+            public string rpn;
+            public double result;
         }
-        else if (precedence.ContainsKey(token))
+        public static void Main()
         {
-            while (operatorStack.Count > 0 &&
-                   precedence.ContainsKey(operatorStack.Peek()) &&
-                   precedence[token] <= precedence[operatorStack.Peek()])
-            {
-                rpn += operatorStack.Pop() + " ";
-            }
-            operatorStack.Push(token);
+            var String = new String();
+            String.input = Console.ReadLine();
+            String.rpn = InfixToRPN(String.input);
+            Console.WriteLine("Выражение в обратной польской записи (ОПЗ):");
+            Console.WriteLine(String.rpn);
+            String.result = CalculateRPN(String.rpn);
+            Console.WriteLine("Результат: " + String.result);
         }
-        else if (token == "(")
+
+        public class varInInfix
         {
-            operatorStack.Push(token);
+            public string[] tokens;
+            public string rpn;
+            public Stack<string> operatorStack;
+            public Dictionary<string, int> precedence;
         }
-        else if (token == ")")
+        public static string InfixToRPN(string infix)
         {
-            while (operatorStack.Count > 0 && operatorStack.Peek() != "(")
+            var varInInfix = new varInInfix();
+            varInInfix.tokens = infix.Split(' ');
+            varInInfix.rpn = "";
+            varInInfix.operatorStack = new Stack<string>();
+
+            varInInfix.precedence = new Dictionary<string, int>
             {
-                rpn += operatorStack.Pop() + " ";
-            }
-            if (operatorStack.Count > 0 && operatorStack.Peek() == "(")
+                { "+", 1 },
+                { "-", 1 },
+                { "*", 2 },
+                { "/", 2 }
+            };
+
+            foreach (string token in varInInfix.tokens)
             {
-                operatorStack.Pop();
+                if (double.TryParse(token, out double number))
+                {
+                    varInInfix.rpn += number + " ";
+                }
+                else if (varInInfix.precedence.ContainsKey(token))
+                {
+                    while (varInInfix.operatorStack.Count > 0 &&
+                           varInInfix.precedence.ContainsKey(varInInfix.operatorStack.Peek()) &&
+                           varInInfix.precedence[token] <= varInInfix.precedence[varInInfix.operatorStack.Peek()])
+                    {
+                        varInInfix.rpn += varInInfix.operatorStack.Pop() + " ";
+                    }
+                    varInInfix.operatorStack.Push(token);
+                }
+                else if (token == "(")
+                {
+                    varInInfix.operatorStack.Push(token);
+                }
+                else if (token == ")")
+                {
+                    while (varInInfix.operatorStack.Count > 0 && varInInfix.operatorStack.Peek() != "(")
+                    {
+                        varInInfix.rpn += varInInfix.operatorStack.Pop() + " ";
+                    }
+                    if (varInInfix.operatorStack.Count > 0 && varInInfix.operatorStack.Peek() == "(")
+                    {
+                        varInInfix.operatorStack.Pop();
+                    }
+                }
             }
+
+            while (varInInfix.operatorStack.Count > 0)
+            {
+                varInInfix.rpn += varInInfix.operatorStack.Pop() + " ";
+            }
+
+            return varInInfix.rpn;
+        }
+
+        public class varInCalculateRPN
+        {
+            public string[] tokens;
+            public Stack<double> numberStack;
+            public double operand2;
+            public double operand1;
+        }
+        public static double CalculateRPN(string rpn)
+        {
+            var varInCalculateRPN = new varInCalculateRPN();
+            varInCalculateRPN.tokens = rpn.Split(' ');
+            varInCalculateRPN.numberStack = new Stack<double>();
+
+            foreach (string token in varInCalculateRPN.tokens)
+            {
+                if (double.TryParse(token, out double number))
+                {
+                    varInCalculateRPN.numberStack.Push(number);
+                }
+                else if (varInCalculateRPN.numberStack.Count >= 2)
+                {
+                    double operand2 = varInCalculateRPN.numberStack.Pop();
+                    double operand1 = varInCalculateRPN.numberStack.Pop();
+
+
+                    switch (token)
+                    {
+                        case "+":
+                            varInCalculateRPN.numberStack.Push(operand1 + operand2);
+                            break;
+                        case "-":
+                            varInCalculateRPN.numberStack.Push(operand1 - operand2);
+                            break;
+                        case "*":
+                            varInCalculateRPN.numberStack.Push(operand1 * operand2);
+                            break;
+                        case "/":
+                            varInCalculateRPN.numberStack.Push(operand1 / operand2);
+                            break;
+                    }
+                }
+            }
+            return varInCalculateRPN.numberStack.Count > 0 ? varInCalculateRPN.numberStack.Peek() : 0;
         }
     }
-
-    while (operatorStack.Count > 0)
-    {
-        rpn += operatorStack.Pop() + " ";
-    }
-
-    return rpn;
-}
-
-// Вычисление выражения в ОПЗ
-static double CalculateRPN(string rpn)
-{
-    string[] tokens = rpn.Split(' ');
-    Stack<double> numberStack = new Stack<double>();
-
-    foreach (string token in tokens)
-    {
-        if (double.TryParse(token, out double number))
-        {
-            numberStack.Push(number);
-        }
-        else if (numberStack.Count >= 2)
-        {
-            double operand2 = numberStack.Pop();
-            double operand1 = numberStack.Pop();
-
-            switch (token)
-            {
-                case "+":
-                    numberStack.Push(operand1 + operand2);
-                    break;
-                case "-":
-                    numberStack.Push(operand1 - operand2);
-                    break;
-                case "*":
-                    numberStack.Push(operand1 * operand2);
-                    break;
-                case "/":
-                    numberStack.Push(operand1 / operand2);
-                    break;
-            }
-        }
-    }
-
-    return numberStack.Count > 0 ? numberStack.Peek() : 0;
 }
